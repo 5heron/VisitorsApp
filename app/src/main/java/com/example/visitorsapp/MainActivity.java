e5cc3c43f6d8d046c63741e79e43cfea082544b2
Sheron <sheronroy2003@gmail.com>
2024-12-18 15:11:02 +0530

    Main menu view created

--- app/src/main/java/com/example/visitorsapp/MainActivity.java
+++ app/src/main/java/com/example/visitorsapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.visitorsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b1,b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,22 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        b1=(Button) findViewById(R.id.addbt);
+        b2=(Button) findViewById(R.id.visbt);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ob = new Intent(getApplicationContext(), AddVisitorsActivity.class);
+                startActivity(ob);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ob = new Intent(getApplicationContext(), ViewVisitorsActivity.class);
+                startActivity(ob);
+            }
         });
     }
 }
