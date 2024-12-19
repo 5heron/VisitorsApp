f61f1dde79369e9a50a459359b082200a86141ba
Sheron <sheronroy2003@gmail.com>
2024-12-19 11:02:31 +0530

    Feature/ViewVisitors updated

--- app/src/main/java/com/example/visitorsapp/ViewVisitorsActivity.java
+++ app/src/main/java/com/example/visitorsapp/ViewVisitorsActivity.java
@@ -1,6 +1,7 @@
 package com.example.visitorsapp;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +10,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ViewVisitorsActivity extends AppCompatActivity {
-
+    TextView fnametxt,lnametxt,purposetxt,whomeettxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +21,9 @@ public class ViewVisitorsActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        fnametxt=(TextView) findViewById(R.id.fname);
+        lnametxt=(TextView) findViewById(R.id.lname);
+        purposetxt=(TextView) findViewById(R.id.purpose);
+        whomeettxt=(TextView) findViewById(R.id.whomeet);
     }
 }
